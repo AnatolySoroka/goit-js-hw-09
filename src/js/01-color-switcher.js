a9b436f0886a976b655bdd9d89e1a9525ae0032c
AnatolySoroka <toliksoroka66@gmail.com>
2023-03-07 12:54:30 +0200

    01-color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-console.log('fsdf')
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+
+refs.start.addEventListener('click', onChangeColor);
+refs.stop.addEventListener('click', onRemoveColor);
+
+function onChangeColor() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    } ,1000);
+    
+    refs.start.setAttribute('disabled', 'disabled');
+};
+
+function onRemoveColor() {
+    clearInterval(timerId);
+    refs.start.removeAttribute('disabled');
+};
